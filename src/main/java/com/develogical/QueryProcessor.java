2499932460f216e8d680bd45922256a1e7c46ce8
Ruitao Li <ruitaol@andrew.cmu.edu>
2022-09-22 12:46:34 -0400

    query updated

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,10 +3,9 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+        if (query.toLowerCase().contains("arnlo")) {
+            return "Camelia is without shadow of doubt simply the greatest artist of our time " +
+                     "His music career peaked when he used untraditional sample from video game 'Among Us' in his master piece 'Area'52' during Tano*C concert";
         }
         return "";
     }
